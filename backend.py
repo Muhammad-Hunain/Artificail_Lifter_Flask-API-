e5e66c4152ff0dac0c0636d090729ae3253c321b
Muhammad-hunain <hunain29.mem@gmail.com>
2024-10-20 19:52:00 +0500

    done

--- backend.py
+++ backend.py
@@ -7,7 +7,7 @@ from sklearn.preprocessing import LabelEncoder
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app, resources={r"/predict": {"origins": "http://localhost:5173"}})
+CORS(app)
 
 
 df = pd.read_excel('DATA_ALMODEL.xlsx') 
